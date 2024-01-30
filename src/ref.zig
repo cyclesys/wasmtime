3691aa0ceaa396f9b82de30818a977f3190137ab
Diego Covarrubias <dcov@pm.me>
2024-01-29 22:36:18 -0800

    finish implementing wrappers for trap types, and add MakeRef* types

--- src/ref.zig
+++ src/ref.zig
@@ -1,27 +1,124 @@
-const c = @cImport(@cInclude("wasmtime.h"));
+const c = @cImport(@cInclude("wasmtime/wasmtime.h"));
+const lib = @import("lib.zig");
 
+/// A reference type: either a funcref or an externref.
 pub const Ref = opaque {
-    pub fn delete(r: *Ref) void {
-        c.wasm_ref_delete(@ptrCast(r));
-    }
-
-    pub fn copy(r: *const Ref) *Ref {
-        return c.wasm_ref_copy(@ptrCast(r));
-    }
-
-    pub fn same(l: *const Ref, r: *const Ref) bool {
-        return c.wasm_ref_same(@ptrCast(l), @ptrCast(r));
-    }
-
-    pub fn getHostInfo(r: *const Ref) *anyopaque {
-        return c.wasm_ref_get_host_info(@ptrCast(r));
-    }
-
-    pub fn setHostInfo(r: *Ref, ptr: *anyopaque) void {
-        c.wasm_ref_set_host_info(@ptrCast(r), ptr);
-    }
-
-    pub fn setHostInfoWithFinalizer(r: *Ref, ptr: *anyopaque, f: fn (*anyopaque) void) void {
-        c.wasm_ref_set_host_info_with_finalizer(@ptrCast(r), ptr, f);
-    }
+    pub usingnamespace MakeRefBase(Ref);
 };
+
+pub fn MakeRefBase(comptime T: type, comptime name: []const u8) type {
+    return struct {
+        /// Copies a value of this type to a new one.
+        ///
+        /// The caller is reponsible for deleting the returned value.
+        pub fn copy(t: *const T) *T {
+            const f = decl("copy");
+            return @ptrCast(f(@ptrCast(t)));
+        }
+
+        /// Are the given references pointing to the same externref?
+        ///
+        /// Note: Wasmtime does not support checking funcrefs for equality, and this
+        /// function will always return false for funcrefs.
+        ///
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn same(l: *const T, r: *const T) bool {
+            const f = decl("same");
+            return f(@ptrCast(l), @ptrCast(r));
+        }
+
+        /// Unimplemented for `Ref` in Wasmtime, always returns `null`.
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn getHostInfo(t: *const T) ?*anyopaque {
+            const f = decl("get_host_info");
+            return f(@ptrCast(t));
+        }
+
+        /// Unimplemented for `Ref` in Wasmtime, aborts the process if called.
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn setHostInfo(t: *T, ptr: *anyopaque) void {
+            const f = decl("set_host_info");
+            f(@ptrCast(t), ptr);
+        }
+
+        /// Unimplemented for `Ref` in Wasmtime, aborts the process if called.
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn setHostInfoWithFinalizer(t: *T, ptr: *anyopaque, finalizer: fn (*anyopaque) void) void {
+            const f = decl("set_host_info_with_finalizer");
+            f(@ptrCast(t), ptr, finalizer);
+        }
+
+        fn decl(comptime qualifier: []const u8) @TypeOf(@field(c, ident(qualifier))) {
+            return @field(c, ident(qualifier));
+        }
+
+        fn ident(comptime qualifier: []const u8) []const u8 {
+            return "wasm_" ++ name ++ "_" ++ qualifier;
+        }
+    };
+}
+
+pub fn MakeRef(comptime T: type, comptime name: []const u8) type {
+    return struct {
+        const base = MakeRefBase(T, name);
+        pub usingnamespace base;
+
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn fromRef(r: *Ref) *T {
+            const f = @field(c, "wasm_ref_as_" ++ name);
+            return @ptrCast(f(@ptrCast(r)));
+        }
+
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn fromRefConst(r: *const Ref) *const T {
+            const f = @field(c, "wasm_ref_as_" ++ name ++ "_const");
+            return @ptrCast(f(@ptrCast(r)));
+        }
+
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn asRef(t: *T) *Ref {
+            const f = base.decl("as_ref");
+            return @ptrCast(f(@ptrCast(t)));
+        }
+
+        /// Unimplemented for `Trap` in Wasmtime, aborts the process if called.
+        pub fn asRefConst(t: *const T) *const Ref {
+            const f = base.decl("as_ref_const");
+            return @ptrCast(f(@ptrCast(t)));
+        }
+    };
+}
+
+pub fn MakeShareableRef(comptime T: type, comptime SharedT: type, comptime name: []const u8) type {
+    return struct {
+        const base = MakeRef(T, name);
+        pub usingnamespace base;
+
+        /// Creates a shareable value from the provided value.
+        ///
+        /// This function does not take ownership of the argument, but the caller is
+        /// expected to deallocate the returned value.
+        ///
+        /// > Note that this API is not necessary in Wasmtime because `Module` can
+        /// > be shared across threads. This is implemented for compatibility, however.
+        pub fn share(t: *const T) *SharedT {
+            const f = base.decl("share");
+            return @ptrCast(f(@ptrCast(t)));
+        }
+
+        /// * Attempts to create a non-shareable value from the shareable value.
+        /// *
+        /// * This function does not take ownership of its arguments, but the caller is
+        /// * expected to deallocate the returned value.
+        /// *
+        /// * This function may fail if the engines associated with the `Store` or
+        /// * the value are different.
+        /// *
+        /// * > Note that this API is not necessary in Wasmtime because `Module` can
+        /// * > be shared across threads. This is implemented for compatibility, however.
+        pub fn obtain(store: *lib.Store, t: *const SharedT) *T {
+            const f = base.decl("obtain");
+            return @ptrCast(f(@ptrCast(store), @ptrCast(t)));
+        }
+    };
+}
--- src/trap.zig
+++ src/trap.zig
@@ -1,9 +1,10 @@
+const c = @cImport(@cInclude("wasmtime/wasmtime.h"));
 const lib = @import("lib.zig");
-const c = @cImport(@cInclude("wasmtime.h"));
+const ref = @import("ref.zig");
 
 /// Opaque struct representing a frame of a wasm stack trace.
 pub const Frame = opaque {
-    /// Destroys the frame
+    /// Deletes the frame
     pub fn delete(f: *Frame) void {
         c.wasm_frame_delete(@ptrCast(f));
     }
@@ -32,35 +33,144 @@ pub const Frame = opaque {
         return c.wasm_frame_func_offset(@ptrCast(f));
     }
 
+    /// Returns a human-readable name for this frame's function.
+    ///
+    /// This function will attempt to load a human-readable name for function this
+    /// frame points to. This function may return `null`.
+    ///
+    /// The lifetime of the returned name is the same as the `Frame` itself.
+    pub fn funcName(f: *const Frame) ?*const lib.Name {
+        return @ptrCast(c.wasmtime_frame_func_name(@ptrCast(f)));
+    }
+
     /// Returns the byte offset from the beginning of the original wasm file
     /// to the instruction this frame points to.
     pub fn moduleOffset(f: *const Frame) usize {
         return c.wasm_frame_module_offset(@ptrCast(f));
     }
+
+    /// Returns a human-readable name for this frame's module.
+    ///
+    /// This function will attempt to load a human-readable name for module this
+    /// frame points to. This function may return `null`.
+    ///
+    /// The lifetime of the returned name is the same as the `Frame` itself.
+    pub fn moduleName(f: *const Frame) ?*const lib.Name {
+        return @ptrCast(c.wasmtime_frame_module_name(@ptrCast(f)));
+    }
 };
 
+/// Opaque struct representing a wasm trap.
 pub const Trap = opaque {
+    /// Code of an instruction trap.
+    pub const Code = enum {
+        /// The current stack space was exhausted.
+        stack_overflow,
+
+        /// An out-of-bounds memory access.
+        memory_out_of_bounds,
+
+        /// A wasm atomic operation was presented with a not-naturally-aligned
+        /// linear-memory address.
+        heap_misaligned,
+
+        /// An out-of-bounds access to a table.
+        table_out_of_bounds,
+
+        /// Indirect call to a null table entry.
+        indirect_call_to_null,
+
+        /// Signature mismatch on indirect call.
+        bad_signature,
+
+        /// An integer arithmetic operation caused an overflow.
+        integer_overflow,
+
+        /// An integer division by zero.
+        integer_division_by_zero,
+
+        /// Failed float-to-int conversion.
+        bad_conversion_to_integer,
+
+        /// Code that was supposed to have been unreachable was reached.
+        unreachable_code_reached,
+
+        /// Execution has potentially run too long and may be interrupted.
+        interrupt,
+
+        /// Execution has run out of the configured fuel amount.
+        out_of_fuel,
+    };
+
+    /// Creates a new trap.
+    ///
+    /// The `Trap` returned is owned by the caller.
+    pub fn new(msg: []const u8) *Trap {
+        return @ptrCast(c.wasmtime_trap_new(@ptrCast(msg.ptr), msg.len));
+    }
+
+    /// Creates a new trap with the provided message.
+    ///
+    /// This function will create a new trap within the given `Store` with the
+    /// provided message. This will also capture the backtrace, if any, of wasm
+    /// frames on the stack.
+    ///
+    /// This function does not take ownership of either argument.
+    ///
+    /// The caller is responsible for deallocating the trap returned.
+    pub fn newWithStore(store: *lib.Store, msg: [:0]const u8) *Trap {
+        return @ptrCast(c.wasm_trap_new(@ptrCast(store), @ptrCast(msg)));
+    }
+
+    /// Deletes the trap
     pub fn delete(t: *Trap) void {
         c.wasm_trap_delete(@ptrCast(t));
     }
 
-    pub fn copy(t: *const Trap) *Trap {
-        return c.wasm_trap_copy(@ptrCast(t));
+    /// Attempts to extract the trap code from this trap.
+    ///
+    /// Returns a `Code` if the trap is an instruction trap triggered while
+    /// executing Wasm. If `null` is returned then this is not
+    /// an instruction trap -- traps can also be created using `Trap.new`,
+    /// or occur with WASI modules exiting with a certain exit code.
+    pub fn code(t: *const Trap) ?Code {
+        var out: c.wasmtime_trap_code_t = undefined;
+        if (c.wasmtime_trap_code(@ptrCast(t), &out)) {
+            return @enumFromInt(out);
+        }
+        return null;
     }
 
-    pub fn same(left: *const Trap, right: *const Trap) bool {
-        return c.wasm_trap_same(@ptrCast(left), @ptrCast(right));
+    /// Retrieves the message associated with this trap.
+    ///
+    /// The caller takes ownership of the returned `Message` value and is responsible for
+    /// calling `Message.delete` on it.
+    pub fn message(t: *const Trap) lib.Message {
+        var msg: lib.Message = undefined;
+        c.wasm_trap_message(@ptrCast(t), @ptrCast(&msg));
+        return msg;
     }
 
-    pub fn getHostInfo(t: *const Trap) *anyopaque {
-        return c.wasm_trap_get_host_info(@ptrCast(t));
+    /// Returns the top frame of the wasm stack responsible for this trap.
+    ///
+    /// The caller is responsible for deallocating the returned frame. This function
+    /// may return `null`, for example, for traps created when there wasn't anything
+    /// on the wasm stack.
+    pub fn origin(t: *const Trap) ?*Frame {
+        return @ptrCast(c.wasm_trap_origin(@ptrCast(t)));
     }
 
-    pub fn setHostInfo(t: *Trap, ptr: *anyopaque) void {
-        c.wasm_trap_set_host_info(@ptrCast(t), ptr);
+    /// Returns the trace of wasm frames for this trap.
+    ///
+    /// The caller is responsible for deallocating the returned list of frames.
+    /// Frames are listed in order of increasing depth, with the most recently called
+    /// function at the front of the list and the base function on the stack at the
+    /// end.
+    pub fn trace(t: *const Trap) lib.FrameVec {
+        var fv: lib.FrameVec = undefined;
+        c.wasm_trap_trace(@ptrCast(t), @ptrCast(&fv));
+        return fv;
     }
 
-    pub fn setHostInfoWithFinalizer(t: *Trap, ptr: *anyopaque, f: *const fn (*anyopaque) callconv(.C) void) void {
-        c.wasm_trap_set_host_info_with_finalizer(@ptrCast(t), ptr, f);
-    }
+    pub usingnamespace ref.MakeRef(Trap);
 };
